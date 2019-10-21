348180e742ca140672c0f6b917c94859665c4e3e
kleysonc castro <kleysoncastro99@gmail.com>
2019-10-21 13:33:10 -0300

    add funcionais

---
+++ sequelize-config/database.js
@@ -0,0 +1,15 @@
+module.exports =  {
+
+    username: "postgres",
+    password: "docker",
+    database: "teste",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+      },
+ 
+}
+
